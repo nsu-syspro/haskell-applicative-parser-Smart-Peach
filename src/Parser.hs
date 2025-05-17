8e12086e61940f3bb27b85bf73b0afc90d0f45bc
Taisia Kashintseva <the_smartest_peach@mail.ru>
2025-05-17 21:06:18 +0700

    Parser done

--- src/Parser.hs
+++ src/Parser.hs
@@ -21,6 +21,7 @@ module Parser
   ) where
 
 import Control.Applicative
+import Control.Monad (ap)
 
 -- | Value annotated with position of parsed input starting from 0
 data Position a = Position Int a
@@ -46,23 +47,49 @@ newtype Parser a = Parser { runParser :: Input -> Parsed a }
 
 -- | Runs given 'Parser' on given input string
 parse :: Parser a -> String -> Parsed a
-parse = error "TODO: define parse"
+parse (Parser p) s = p $ Position 0 s
 
 -- | Runs given 'Parser' on given input string with erasure of @Parsed a@ to @Maybe a@
 parseMaybe :: Parser a -> String -> Maybe a
-parseMaybe = error "TODO: define parseMaybe"
+parseMaybe p s = case parse p s of
+                    Parsed a _ -> Just a
+                    _          -> Nothing
+
+instance Functor Parsed where
+  fmap f (Parsed a input) = Parsed (f a) input
+  fmap _ (Failed err) = Failed err
+
+instance Applicative Parsed where -- Seem stupid
+  pure i = Parsed i undefined
+  (<*>) (Parsed f s1) (Parsed a _) = Parsed (f a) s1 -- seems wrong
+  (<*>) _ (Failed err) = Failed err 
+  (<*>) (Failed err) _ = Failed err 
+
+instance Alternative Parsed where
+  empty = Failed []
+  (<|>) (Parsed a s) _ = Parsed a s
+  (<|>) _ (Parsed a s) = Parsed a s
+  (<|>) (Failed err1) (Failed err2) = Failed $ err1 ++ err2
 
 instance Functor Parser where
-  fmap = error "TODO: define fmap (Parser)"
+  -- fmap f (Parser p) = Parser $ fmap f . p
+  fmap f p = pure f <*> p
+
+instance Monad Parser where 
+  (Parser p) >>= f = Parser $ \input -> 
+    case p input of
+      Parsed val input' -> runParser (f val) input'
+      Failed err     -> Failed err
 
 instance Applicative Parser where
-  pure = error "TODO: define pure (Parser)"
-  (<*>) = error " TODO: define <*> (Parser)"
+  pure value = Parser(Parsed value)
+  (<*>) = ap -- is it considered cheating?
+
 
 instance Alternative Parser where
-  empty = error "TODO: define empty (Parser)"
+  empty = Parser (const (Failed []))
   -- Note: when both parsers fail, their errors are accumulated and *deduplicated* to simplify debugging
-  (<|>) = error " TODO: define <|> (Parser)"
+  (<|>) (Parser a) (Parser b) = Parser $ \s -> a s <|> b s
 
 -- | Parses single character satisfying given predicate
 --
@@ -78,4 +105,10 @@ instance Alternative Parser where
 -- Failed [Position 0 EndOfInput]
 --
 satisfy :: (Char -> Bool) -> Parser Char
-satisfy = error "TODO: define satisfy"
+satisfy predicate = Parser $ \input ->
+  case input of
+  Position pos (c : rest) | predicate c -> Parsed c (Position (pos + 1) rest)
+  Position pos (c : _) -> Failed [Position pos (Unexpected c)]
+  Position pos [] -> Failed [Position pos EndOfInput]
+
+                                    
